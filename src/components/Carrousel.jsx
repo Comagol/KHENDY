b8f625d7a79221a17ac994c107d9e3e5819276fa
Comagol <comabellafrancisco@gmail.com>
2025-01-21 17:44:01 -0300

    Creation of Carrousel.jsx and implementing it on home

---
+++ src/components/Carrousel.jsx
@@ -0,0 +1,77 @@
+import { Box, IconButton } from "@chakra-ui/react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const images = [
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQe5Vg7WDkIka6XQ0to9iEYumlHFSoY0IkQWrJhlnQzFyBWJmwJkzpInOAnoOOa4UBLMYU&usqp=CAU",
+    "https://media.istockphoto.com/id/482779158/es/foto/estilo-boho-girls-hands-looking-femenina-con-muchos-anillos.jpg?s=612x612&w=0&k=20&c=Zk3oPf_Di-qKIgrFP3Jvf0ZCBHJp-axrBLQ37o5ewnY=",
+    "https://acdn.mitiendanube.com/stores/001/419/929/products/img_20230303_2112361-1ade31b269188e65a916778906133433-1024-1024.jpg",
+    "https://acdn.mitiendanube.com/stores/001/019/611/products/279101341_715579946452850_1402280651726738510_n1-7f797d3520c0194ab816510924542511-240-0.jpg"
+  ];
+
+const Carrousel = () => {
+  return (
+    <Box position="relative" width="full" maxW="800px" mx="auto">
+      {/* Botón para ir atrás */}
+      <IconButton
+        aria-label="Previous Slide"
+        icon={<ChevronLeftIcon />}
+        position="absolute"
+        left="10px"
+        top="50%"
+        transform="translateY(-50%)"
+        zIndex="10"
+        bg="rgba(0, 0, 0, 0.5)"
+        color="white"
+        _hover={{ bg: "black" }}
+        className="swiper-button-prev"
+      />
+
+      {/* Swiper */}
+      <Swiper
+        modules={[Navigation, Pagination]}
+        navigation={{
+          prevEl: ".swiper-button-prev",
+          nextEl: ".swiper-button-next"
+        }}
+        pagination={{ clickable: true }}
+        loop={true}
+        slidesPerView={1}
+      >
+        {images.map((src, index) => (
+          <SwiperSlide key={index}>
+            <Box
+              as="img"
+              src={src}
+              alt={`Slide ${index + 1}`}
+              width="100%"
+              height="auto"
+              borderRadius="md"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      {/* Botón para ir adelante */}
+      <IconButton
+        aria-label="Next Slide"
+        icon={<ChevronRightIcon />}
+        position="absolute"
+        right="10px"
+        top="50%"
+        transform="translateY(-50%)"
+        zIndex="10"
+        bg="rgba(0, 0, 0, 0.5)"
+        color="white"
+        _hover={{ bg: "black" }}
+        className="swiper-button-next"
+      />
+    </Box>
+  );
+};
+
+export default Carrousel;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Carrousel from '../components/Carrousel'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Carrousel/>
   )
 }
 
