f7e4550dba2721bb1fbf4e3a2e6fea3c3de717a3
Comagol <comabellafrancisco@gmail.com>
2025-02-02 16:43:21 -0300

    hiding credentials in env.local

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,17 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA_NjEzEAJ8prtbjnguHtjp8cRAao8_G7c",
-  authDomain: "khendy-c4ded.firebaseapp.com",
-  projectId: "khendy-c4ded",
-  storageBucket: "khendy-c4ded.firebasestorage.app",
-  messagingSenderId: "517583267591",
-  appId: "1:517583267591:web:79284513f17b3b39c447a2"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
+// Inicializar Firebase
 export const app = initializeApp(firebaseConfig);
