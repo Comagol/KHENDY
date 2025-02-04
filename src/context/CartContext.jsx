e7a39e3c656c390a8e73ac1d67713b036e6704f8
Comagol <comabellafrancisco@gmail.com>
2025-02-04 18:03:40 -0300

    creating the CartContext to handle quantities added to the cart

---
+++ src/context/CartContext.jsx
@@ -0,0 +1,37 @@
+import { Children, createContext, useContext, useState } from "react";
+
+const CartContext = createContext();
+
+export const CartProvider = ({Children}) => {
+    const [cart, setCart] = useState([]);
+
+    const addToCart = (product, quantity) => {
+        setCart((prevCart) => {
+            const existingProduct = prevCart.find(item) => item.id === product.id;
+            if (existingProduct){
+                return prevCart.map((item) =>
+                    item.id === product.id ? {...item, quantity } : item
+            
+                )
+            } else {
+                return [...prevCart, { ...product, quantity}];
+            }
+        });
+    };
+
+    const updateQuantity = (productId, quantity) => {
+        setCart((prevCart) =>
+            prevCart.map((item) =>
+                item.id === productId ? { ...item, quantity} : item
+            )
+        );
+    };
+
+    return (
+        <CartContext.Provider value = {{cart, addToCart, updateQuantity}}>
+            {children}
+        </CartContext.Provider>
+    );
+};
+
+export const useCart = () => useContext(CartContext);
